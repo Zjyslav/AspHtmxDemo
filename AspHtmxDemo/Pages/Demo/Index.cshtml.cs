93030a602355eaf09d7fbc72b318ba9e9d5c5aa4
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-12-20 14:28:59 +0100

    Add button that gets a new number and adds it to the list in 1 request

--- AspHtmxDemo/Pages/Demo/Index.cshtml.cs
+++ AspHtmxDemo/Pages/Demo/Index.cshtml.cs
@@ -6,15 +6,17 @@ namespace AspHtmxDemo.Pages.Demo;
 public class IndexModel : PageModel
 {
     public int YourNumber { get; set; }
+    public bool IncludeListItem { get; set; } = false;
 
     public void OnGet()
     {
         YourNumber = Random.Shared.Next(1, 100);
     }
 
-    public async Task<PartialViewResult> OnGetYourNumberAsync()
+    public async Task<PartialViewResult> OnGetYourNumberAsync(bool includeListItem)
     {
         OnGet();
+        IncludeListItem = includeListItem;
         await Task.Delay(1000);
         return Partial("_YourNumber", this);
     }
